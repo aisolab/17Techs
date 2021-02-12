91351921c9ac85672d29dab895d4d7df8b310a6b
aisolab <bsk0130@gmail.com>
2021-02-12 19:51:06 +0900

    update ch02

---
+++ ch02_internalization/i18n_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# 다음 코드를 실행하기 위해서는 python-gettext 모듈이 필요합니다.
+import gettext
+
+translation_ko = gettext.translation(
+    domain="i18n_test",
+    localedir="./locale",
+    languages=["ko_KR", "zh_CN", "ja_JP"],
+)
+
+
+translation_ko.install()
+
+print(_("Test message 1"))
+print(_("Test message 2"))
+print(_("Test message 3"))
+
+# http://www.labri.fr/perso/fleury/posts/programming/a-quick-gettext-tutorial.html
